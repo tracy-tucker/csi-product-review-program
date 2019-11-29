98ac50d7623b16251f283ab6adc48c7b81aef2b6
treuther <tracyreuther@gmail.com>
2019-11-29 12:55:31 -0600

    route setup for user login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get '/' => 'sessions#welcome'
   get '/login' => 'sessions#new' #existing user needs a session
   post '/login' => 'sessions#create'
+  get '/signup' => 'users#new' #a new user needs to be created before it has a session
+  post '/signup' => 'users#create'
+
+  get '/auth/google_oauth2/callback' => 'sessions#omniauth'
 
   resources :reviews
   resources :products
