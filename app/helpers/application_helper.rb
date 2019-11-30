a81d4c19375066861674f1901e78545f6756364b
treuther <tracyreuther@gmail.com>
2019-11-30 11:03:44 -0600

    navigation links addition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def render_nav_links
+        if current_user
+            render partial: 'layouts/nav_links'
+        else
+            render partial: 'layouts/logged_out_links'
+        end
+    end
+    
 end
