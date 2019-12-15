a2b74bec41cb64b46ab3febd35c276d4e2724eeb
treuther <tracyreuther@gmail.com>
2019-12-15 05:28:03 -0600

    forms for chem_group and application_area

--- app/controllers/application_areas_controller.rb
+++ app/controllers/application_areas_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationAreasController < ApplicationController
+
+    def index
+        @application_areas = ApplicationArea.all
+    end
+    
+    def new
+        if logged_in?
+            @application_area = ApplicationArea.new
+        else
+            flash[:error] = "Sorry, you must be logged in to create a new chemical group."
+            redirect_to application_areas_path
+        end
+    end
+
+    def create
+        @application_area = ApplicationArea.new(application_area_params)
+        # @application_area.user_id = session[:user_id] #bc application_area belongs_to user. user_id required from model
+        if @application_area.save #validation
+            redirect_to application_areas_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def application_area_params
+        params.require(:application_area).permit(:area_name)
+    end
+
 end
--- app/controllers/chem_groups_controller.rb
+++ app/controllers/chem_groups_controller.rb
@@ -7,14 +7,17 @@ class ChemGroupsController < ApplicationController
     def new
         if logged_in?
             @chem_group = ChemGroup.new
+        else
+            flash[:error] = "Sorry, you must be logged in to create a new chemical group."
+            redirect_to chem_groups_path
         end
     end
 
     def create
         @chem_group = ChemGroup.new(chem_group_params)
-        @chem_group.user_id = session[:user_id] #bc product belongs_to user. user_id required from model
+        # @chem_group.user_id = session[:user_id] #bc chem_group belongs_to user. user_id required from model
         if @chem_group.save #validation
-            redirect_to products_path
+            redirect_to chem_groups_path
         else
             render :new
         end
